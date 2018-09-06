19224e4c5417368c088d010fef42f770cfaf59c1
Yutthapichai <cancel_go@hotmail.com>
2018-09-06 08:05:54 +0700

    Finished blog by nuxt framwork

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 const pkg = require('./package')
+//const bodyParser = require("body-parser")
 
 module.exports = {
   mode: "universal",
@@ -73,4 +74,5 @@ module.exports = {
   //router: {
   //  middleware: 'log'
   //}
+  //serverMiddleware: [bodyParser.json(),"~/api"]
 };
